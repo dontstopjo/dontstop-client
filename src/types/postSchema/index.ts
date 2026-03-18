5f7b282d8f2695b66e5b622d5634fd3fe83d5655
j02on <pjylove08@naver.com>
2026-03-18 11:54:36 +0900

    feat : type 정리

---
+++ src/types/postSchema/index.ts
@@ -0,0 +1,10 @@
+export interface PostSchemaType {
+  id: number;
+  authorName: string;
+  title: string;
+  keyword: string[];
+  description: string;
+  views: number;
+  likes: number;
+  imgURL: string;
+}
