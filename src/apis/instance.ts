e2f01379462d8dab8415a9391211ac47468e9cb5
김소희 <163392880+ttohee@users.noreply.github.com>
2026-04-16 19:03:21 +0900

    feat: 이미지 URL 수정

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -19,8 +19,24 @@ instance.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
+const toHttps = (obj: unknown): unknown => {
+  if (typeof obj === 'string') {
+    return obj.startsWith('http://') ? obj.replace('http://', 'https://') : obj;
+  }
+  if (Array.isArray(obj)) return obj.map(toHttps);
+  if (obj && typeof obj === 'object') {
+    return Object.fromEntries(
+      Object.entries(obj).map(([k, v]) => [k, toHttps(v)])
+    );
+  }
+  return obj;
+};
+
 instance.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    response.data = toHttps(response.data);
+    return response;
+  },
   (error) => {
     return Promise.reject(error);
   },
